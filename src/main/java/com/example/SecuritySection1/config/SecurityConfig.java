06c5c72e71e7913456c44437ddd26cf29f22ed10
Debabrata <das.debabrata99@gmail.com>
2025-08-30 06:09:58 +0530

    3. Using bcrypt password

--- src/main/java/com/example/SecuritySection1/config/SecurityConfig.java
+++ src/main/java/com/example/SecuritySection1/config/SecurityConfig.java
@@ -8,7 +8,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -30,13 +33,16 @@ public class SecurityConfig {
     @Bean
     public UserDetailsService createUserDetails() {
     	UserDetails user = User.withUsername("user").password("{noop}12345").build();
-    	UserDetails admin = User.withUsername("admin").password("{noop}54321").build();
+    	//bcrypt hash generated for password "54321" using th website "https://bcrypt-generator.com/"
+    	UserDetails admin = User.withUsername("admin").password("{bcrypt}$2a$12$QdReAGY3RcaUCAXh3h7DceJ3YAJ/GfPrLmfaFWRa5LezgKRoaV3Ae").build();
     	return new InMemoryUserDetailsManager(user, admin);
     }
     
-//    @Bean
-//    public PasswordEncoder createPasswordEncoder()
-    
+    @Bean
+    public PasswordEncoder createPasswordEncoder() {
+    	u
+    	return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
     
 }
 
